36feb15f0c7e984bee4d3d998846957eedb0c8b1
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 19:59:20 +0200

    Update Rectangulo.php

--- Lv.1/Ej.2/Rectangulo.php
+++ Lv.1/Ej.2/Rectangulo.php
@@ -3,10 +3,7 @@ declare(strict_types=1);
 
 
 class Rectangulo extends Shape{
-    public function __construct(int $altura, int $ancho){
-        parent::__construct($altura, $ancho);
-    }
-
+    
     public function calcularArea(): int{
         return $this->altura * $this->ancho;
     }
