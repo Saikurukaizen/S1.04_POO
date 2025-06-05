bdd5d07c5cc6af6fe2d9a5aad9d4128fc12e4249
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 19:59:32 +0200

    Update Triangulo.php

--- Lv.1/Ej.2/Triangulo.php
+++ Lv.1/Ej.2/Triangulo.php
@@ -2,11 +2,7 @@
 declare(strict_types=1);
 
 class Triangulo extends Shape{
-
-    public function __construct(int $altura, int $ancho){
-        parent::__construct($altura, $ancho);
-    }
-
+    
     public function calcularArea(): int{
         return ($this->altura * $this->ancho) / 2;
     }
